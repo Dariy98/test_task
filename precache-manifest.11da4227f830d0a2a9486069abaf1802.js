dda596a2556cfb501622bfd7222cb29f352048aa
Dariy98 <ostapenko.d98@gmail.com>
2020-08-15 12:09:39 +0300

    Updates

---
+++ precache-manifest.11da4227f830d0a2a9486069abaf1802.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f76b130051f64df86efcdf83a270e2a6",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b5f7bff2361f25bd5238",
+    "url": "/my-app/static/css/main.3b1b07f2.chunk.css"
+  },
+  {
+    "revision": "ae4a07ce55f1ed83d5d6",
+    "url": "/my-app/static/js/2.0a8f2d3a.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/my-app/static/js/2.0a8f2d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f7bff2361f25bd5238",
+    "url": "/my-app/static/js/main.54696f5d.chunk.js"
+  },
+  {
+    "revision": "fa475cd0ed59fa871de4",
+    "url": "/my-app/static/js/runtime-main.079fb1f6.js"
+  }
+]);
